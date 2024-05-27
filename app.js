4b7d98ec9ece22188223f6971624d4978feb6791
Админ <macbahodir@gmail.com>
2024-05-27 12:09:25 +0500

    My homework in JS

---
+++ app.js
@@ -0,0 +1,97 @@
+// FOR sikli bo'yicha
+
+
+//for 1
+
+// k = 5;
+// n = 7;
+// for(i = 1; i <= n; i++) {
+//     console.log(k);
+// }
+
+//for 2
+
+// let a = 5;
+// let b = 8;
+// let count = 0;
+// for (let i = Math.ceil(Math.min(a, b)); i <= Math.floor(Math.max(a, b)); i++) {
+//     console.log(i);
+//     count++;
+// }
+// console.log("Chiqarilgan sonlar soni: " + count);
+
+//for 3
+
+// let a = 12;
+// let b = 5;
+// let count = 0;
+
+// for (let i = Math.floor(Math.max(a, b)); i >= Math.ceil(Math.min(a, b)); i--) {
+//     console.log(i);
+//     count++;
+// }
+// console.log("Chiqarilgan sonlar soni: " + count);
+
+//for 4
+
+// let narx = 5000;
+// let maxVazn= 10;
+
+// for (let i = 1; i <= maxVazn; i++) {
+//     let hozirginarx = i * narx;
+//     console.log(i + ` kg konfetning narxi: ${hozirginarx}`);
+// }
+
+//for 5
+
+// let price = 5000
+// for (let i = 1; i <= 9; i++) {
+//     let innerPrice = i / 10;
+//     console.log(`${innerPrice} kg konfet narxi: ${innerPrice * 10 + '00'} so'm`);
+// }
+
+//for 6
+
+
+
+//for 7
+
+// let a = 5;
+// let b = 7;
+// let sum = 0;
+
+// for (i = 5; i <= b; i++) {
+//     sum += i
+//     console.log(Math.floor(sum));
+// }
+
+//for 8
+
+// let a = 3;
+// let b = 4;
+
+// for(i = 1; i <= b; i ++) {
+
+// }
+
+
+//13
+
+
+// for(i = 1; i <= 5; i++) {
+//     let randomNum = Math.floor(Math.random() * 10);
+//     console.log(randomNum);
+// }
+
+// 3
+
+// let randomColor = Math.floor(Math.random() * 300);
+// let randomColor2 = Math.floor(Math.random() * 300);
+// let randomColor3 = Math.floor(Math.random() * 100);
+// console.log(`rgb(${randomColor},${randomColor2},${randomColor3})`);
+
+// 5
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`${i} x ${i} = ${i * i}`);
+// }  
